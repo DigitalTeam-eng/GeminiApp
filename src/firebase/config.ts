108914145d550b3d212d4efdbc411f37d7bb8153
Sjællandske Nyheder Digital <DigitalTeam@sn.dk>
2025-10-15 10:57:39 +0000

    https://studio-6932359731-5d066.firebaseapp.com/__/auth/handler?code=1.A

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-6932359731-5d066",
   "appId": "1:132477403200:web:ec43e3967286b4494d5fa5",
   "apiKey": "AIzaSyBXs4Zt7FZI7ibU1pmSuF1LT4_J9mIutJA",
-  "authDomain": "studio-6932359731-5d066.firebaseapp.com",
+  "authDomain": "geminiapp-dxaah3g6cnhadthr.northeurope-01.azurewebsites.net",
   "measurementId": "",
   "messagingSenderId": "132477403200"
 };
