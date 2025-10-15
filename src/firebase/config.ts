638d7eb3fe31b4c371ade3eae84e653168c2cc0b
Sjællandske Nyheder Digital <DigitalTeam@sn.dk>
2025-10-15 06:46:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6932359731-5d066",
+  "appId": "1:132477403200:web:ec43e3967286b4494d5fa5",
+  "apiKey": "AIzaSyBXs4Zt7FZI7ibU1pmSuF1LT4_J9mIutJA",
+  "authDomain": "studio-6932359731-5d066.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "132477403200"
+};
