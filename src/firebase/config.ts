faedb98f75acb15522b1eca6e92d82187105bd6c
Sjællandske Nyheder Digital <DigitalTeam@sn.dk>
2025-10-15 12:07:00 +0000

    stadig 404 i deployet version i azure

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+// This file is intentionally minimal to force the Firebase SDK to use the specified authDomain.
+// By providing only the essential keys, we prevent the SDK from falling back to default 
+// or auto-generated configurations that might lead to incorrect authentication paths on Azure.
+
 export const firebaseConfig = {
-  "projectId": "studio-6932359731-5d066",
-  "appId": "1:132477403200:web:ec43e3967286b4494d5fa5",
   "apiKey": "AIzaSyBXs4Zt7FZI7ibU1pmSuF1LT4_J9mIutJA",
   "authDomain": "geminiapp-dxaah3g6cnhadthr.northeurope-01.azurewebsites.net",
-  "measurementId": "",
-  "messagingSenderId": "132477403200"
+  "projectId": "studio-6932359731-5d066",
 };
