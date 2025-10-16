38aa8731cc1040f15147786d30e0d5b50b2ccb6e
Sjællandske Nyheder Digital <DigitalTeam@sn.dk>
2025-10-16 06:01:06 +0000

    npm build fejl igen

--- next.config.ts
+++ next.config.ts
@@ -43,21 +43,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   webpack: (config, { isServer }) => {
-    // This is to overcome a build issue with genkit/handlebars and next.js
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
